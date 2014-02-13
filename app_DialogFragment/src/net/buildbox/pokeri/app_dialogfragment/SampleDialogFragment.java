64c07cfc8bd4902611a08fbb7369927e155b70f0
shige <shige0501@gmail.com>
2014-02-14 00:04:18 +0900

    Chapter 02 「ダイアログフラグメントを利用する」（P.28）サンプル追加

---
+++ app_DialogFragment/src/net/buildbox/pokeri/app_dialogfragment/SampleDialogFragment.java
@@ -0,0 +1,28 @@
+package net.buildbox.pokeri.app_dialogfragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class SampleDialogFragment extends DialogFragment {
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		
+		// アラートダイアログの構築
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+			.setTitle("サンプルダイアログ")
+			.setMessage("サンプルメッセージ")
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dismiss();
+				}
+			});
+		
+		return builder.create();
+	}
+}
+
