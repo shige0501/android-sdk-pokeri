d424c0748280010d6d4b04233ada80a98ba393e5
shige <shige0501@gmail.com>
2014-02-13 23:20:44 +0900

    Chapter 02
    「アクションバーの概要」にて、ActionBarActivityを使ったプロジェクトを作成したときのサンプルプロジェクト

---
+++ app_ActionBarActivity/src/net/buildbox/pokeri/app_actionbaractivity/MainActivity.java
@@ -0,0 +1,21 @@
+package net.buildbox.pokeri.app_actionbaractivity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
