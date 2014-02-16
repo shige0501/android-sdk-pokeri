7a7228035f4466cec86cafe0467f3c77b4ce8e50
shige <shige0501@gmail.com>
2014-02-16 23:39:20 +0900

    Chapter 01 「LinearLayoutを表示する」（P.4） サンプル追加

---
+++ Chapter 01/layout_LinearLayout/gen/net/buildbox/pokeri/layout_linearlayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.buildbox.pokeri.layout_linearlayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
