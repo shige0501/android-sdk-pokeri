0503ffb893932be35804dbe06c891c97a7ec4135
shige <shige0501@gmail.com>
2014-02-16 23:40:52 +0900

    不要なフォルダとファイルを削除
    Chapter 01 「LinearLayoutを表示する」

--- Chapter 01/layout_LinearLayout/gen/net/buildbox/pokeri/layout_linearlayout/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.buildbox.pokeri.layout_linearlayout;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
