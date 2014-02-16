70b23c48f95c280c619c3f49cb4c671ef477b734
shige <shige0501@gmail.com>
2014-02-16 18:26:20 +0900

    Chapter 09 「Wi-Fiの状態変化を検知する」（P.386） サンプル追加

---
+++ Chapter 09/device_WifiState/src/net/buildbox/pokeri/device_wifistate/MainActivity.java
@@ -0,0 +1,56 @@
+package net.buildbox.pokeri.device_wifistate;
+
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+        // Wi-Fiの有効状態取得
+        WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+        boolean bEnabled = wifiMgr.isWifiEnabled();
+        
+        TextView tv_wifiEnabled = (TextView) findViewById(R.id.tv_wifiEnabled);
+        tv_wifiEnabled.setText("Wi-Fiの有効状態: " + (bEnabled? "true" : "false") );
+        
+        // Wi-Fiの状態を取得
+        int wifiState = wifiMgr.getWifiState();
+        TextView tv_wifiState = (TextView) findViewById(R.id.tv_wifiState);
+        switch (wifiState) {
+        case WifiManager.WIFI_STATE_DISABLING:
+        	// 無効化中
+        	tv_wifiState.setText("Wi-Fiの状態： 無効化中");
+        	break;
+        case WifiManager.WIFI_STATE_DISABLED:
+        	// 無効状態
+        	tv_wifiState.setText("Wi-Fiの状態： 無効");
+        	break;
+        case WifiManager.WIFI_STATE_ENABLING:
+        	// 有効化中
+        	tv_wifiState.setText("Wi-Fiの状態: 有効化中");
+        	break;
+        case WifiManager.WIFI_STATE_ENABLED:
+        	// 有効状態
+        	tv_wifiState.setText("Wi-Fiの状態： 有効");
+        	break;
+        case WifiManager.WIFI_STATE_UNKNOWN:
+        	// 不定
+        	tv_wifiState.setText("Wi-Fiの状態: 不明");
+        	break;
+        }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
