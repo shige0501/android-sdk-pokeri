339f0d8bce9629849dfccee4cff2fb34ccf6a0d5
shige <shige0501@gmail.com>
2014-02-17 22:13:04 +0900

    Chapter 01 「スペース（空白）を表示する」（P.14） サンプル追加

---
+++ Chapter 01/layout_Space/src/net/buildbox/pokeri/layout_space/MainActivity.java
@@ -0,0 +1,21 @@
+package net.buildbox.pokeri.layout_space;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
