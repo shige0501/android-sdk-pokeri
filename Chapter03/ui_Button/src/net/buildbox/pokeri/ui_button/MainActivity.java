fec3a02686dcfbd5c5ffe4720b8c7c68eec99e32
shige <shige0501@gmail.com>
2014-02-22 22:17:41 +0900

    Chapter03 「ボタンを表示する」（P.70）サンプル追加

---
+++ Chapter03/ui_Button/src/net/buildbox/pokeri/ui_button/MainActivity.java
@@ -0,0 +1,27 @@
+package net.buildbox.pokeri.ui_button;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void onClickEvent(View v) {
+		Toast.makeText(this, "ボタンがクリックされました", Toast.LENGTH_LONG).show();
+	}
+
+}
